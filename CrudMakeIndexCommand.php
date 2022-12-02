59b9892ea4252a5ff9f10f994779e61399c17bdf
Michael <m@cadot.eu>
2022-12-02 13:30:06 +0100

    patch sur les images définis par srcset

--- CrudMakeIndexCommand.php
+++ CrudMakeIndexCommand.php
@@ -46,10 +46,10 @@ class CrudMakeIndexCommand extends Command
             //creation des th
             if (!isset($options['tpl']['no_index']) && $name != 'deletedAt' && $name != 'createdAt' && $name != 'updatedAt') {
                 if (isset($docs->getOptions()['id']['order']) || in_array($docs->getType($name), ['manytomany', 'onetomany'])) {
-                    $th[] = "<th >$name</th>";
+                    $th[] = "<th >$name";
                 } else {
-                    $th[] = "<th {% if pagination.isSorted('a.$name') %} class='sorted'{% endif %}>
-                {{ knp_pagination_sortable(pagination, '$name', 'a.$name') }}</th>";
+                    $th[] = "<th {{pagination.isSorted('a.$name')?\"class='sorted'\"}}>
+                {{ knp_pagination_sortable(pagination, '$name', 'a.$name') }}";
                 }
             }
         }
@@ -62,19 +62,19 @@ class CrudMakeIndexCommand extends Command
         {%if action=="deleted" %}
             <th>
             <a class="btn btn-outline-primary {{ app.request.query.get(" tri") == 'deletedAt' ? 'active' }} " href='?tri=deletedAt&&ordre={{ app.request.query.get("ordre")=="DESC" ? "ASC":"DESC" }}'>effacé</a>
-            </th>
+            
         {% endif %}
         EOT;
         /* ------------------------- datetime avec knpsorted ------------------------ */
         if (!isset($IDOptions['tpl']['no_created']))
-            $th[] = "<th {% if pagination.isSorted('a.createdAt') %} class='sorted'{% endif %}>
-        {{ knp_pagination_sortable(pagination, 'créé', 'a.createdAt') }}</th>";
+            $th[] = "<th {{pagination.isSorted('a.createdAt')?\"class='sorted'\"}}>
+        {{ knp_pagination_sortable(pagination, 'créé', 'a.createdAt') }}";
         if (!isset($IDOptions['tpl']['no_updated']))
             if (isset($docs->getOptions()['id']['order'])) {
-                $th[] = "<th >Mis à jour</th>";
+                $th[] = "<th >Mis à jour";
             } else {
-                $th[] = "<th {% if pagination.isSorted('a.updatedAt') %} class='sorted'{% endif %}>
-        {{ knp_pagination_sortable(pagination, 'mis à jour', 'a.updatedAt') }}</th>";
+                $th[] = "{{pagination.isSorted('a.updatedAt')?\"class='sorted'\"}}>
+        {{ knp_pagination_sortable(pagination, 'mis à jour', 'a.updatedAt') }}";
             }
         /* ---------------------------------- body ---------------------------------- */
         $tableauChoice = '';
@@ -92,7 +92,7 @@ class CrudMakeIndexCommand extends Command
                 switch ($select = $docs->getSelect($name)) {
                     case 'generatedvalue': //id
                     case 'hiddenroot':
-                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" > {{' . "$Entity.$name$twig" . '}}' . "\n</td>";;
+                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" > {{' . "$Entity.$name$twig" . '}}' . "\n";;
                         break;
                     case 'simple':
                     case 'simplelanguage':
@@ -101,27 +101,27 @@ class CrudMakeIndexCommand extends Command
                     case 'text':
                         $twig = isset($options['twig']) ?  $twig : '|striptags|u.truncate(20, \'...\')';
                         $twigtitle = isset($options['twig']) ? '|' . implode('|', array_keys($options['twig'])) : '|striptags|u.truncate(200, \'...\')';
-                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name$twigtitle" . '}}"> {{' . "$Entity.$name$twig" . '}}' . "\n</td>";
+                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name$twigtitle" . '}}"> {{' . "$Entity.$name$twig" . '}}' . "\n";
                         break;
                     case 'string':
                     case 'email':
                         $twig = isset($options['twig']) ?  $twig : '|striptags|u.truncate(40, \'...\')';
                         $twigtitle = isset($options['twig']) ? '|' . implode('|', array_keys($options['twig'])) : '|striptags|u.truncate(200, \'...\')';
-                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name$twigtitle" . '}}"> {{' . "$Entity.$name$twig" . '}}' . "\n</td>";
+                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name$twigtitle" . '}}"> {{' . "$Entity.$name$twig" . '}}' . "\n";
                         break;
                     case 'drapeau':
                         ///node_modules/flag-icons/flags/1x1/fr.svg
-                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name" . '}}">' . "{% if $Entity.$name %} <img src=\"/build/flags/1x1/{{ $Entity.$name }}.svg\" style=\"max-height:2rem\"> {% endif %} \n</td>";
+                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name" . '}}">' . "{% if $Entity.$name %} <img src=\"/build/flags/1x1/{{ $Entity.$name }}.svg\" style=\"max-height:2rem\"> {% endif %} \n";
                         break;
                     case 'manytoone':
                         $champ = isset($options['options']['champ']) ? $options['options']['champ'] : 'id';
                         $twig = isset($options['twig']) ?  $twig : '|striptags|u.truncate(40, \'...\')';
-                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name?$Entity.$name.$champ" . '}}"> {{' . "$Entity.$name? $Entity.$name.$champ$twig" . '}}' . "\n</td>";
+                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name?$Entity.$name.$champ" . '}}"> {{' . "$Entity.$name? $Entity.$name.$champ$twig" . '}}' . "\n";
                         break;
                     case 'money':
                     case 'choice':
                         $twigtitle = isset($options['twig']) ? '|' . implode('|', array_keys($options['twig'])) : '|striptags|u.truncate(200, \'...\')';
-                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name$twigtitle" . '}}"> {{' . "$Entity.$name$twig" . '}}' . "\n</td>";
+                        $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name$twigtitle" . '}}"> {{' . "$Entity.$name$twig" . '}}' . "\n";
                         break;
                     case 'integer':
                         if (isset($IDOptions['order']) && array_key_exists($name, $IDOptions['order'])) {
@@ -134,10 +134,10 @@ class CrudMakeIndexCommand extends Command
                                     </a>
                                 ";
                             }
-                            $td[] = " <td class='my-auto " . implode(' ', $class) . "' >" . $chaine . "</td>";
+                            $td[] = " <td class='my-auto " . implode(' ', $class) . "' >" . $chaine . "";
                         } else {
-                            $td[] = '<td class="my-auto ' . implode(' ', $class) . '" > {{' . "$Entity.$name$twig" . '}}' . "\n</td>";
-                            $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name$twig" . '}}"> {{' . "$Entity.$name$twig" . '}}' . "\n</td>";
+                            $td[] = '<td class="my-auto ' . implode(' ', $class) . '" > {{' . "$Entity.$name$twig" . '}}' . "\n";
+                            $td[] = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name$twig" . '}}"> {{' . "$Entity.$name$twig" . '}}' . "\n";
                         }
                         break;
                     case 'image':
@@ -152,13 +152,13 @@ class CrudMakeIndexCommand extends Command
                             //retoune que le nom du fichier
                             $tdtemp .= "{% if $Entity.$name is not empty %}<span data-controller='base--bigpicture' data-base--bigpicture-options-value='{\"imgSrc\": \"{{asset($Entity.$name)}}\"}'>{{TBgetFilename($Entity.$name)$twig}}</span> {% endif %}";
                         }
-                        $td[] = $tdtemp . '' . "\n</td>";
+                        $td[] = $tdtemp . '' . "\n";
                         break;
                     case 'fichier':
                         $tdtemp = '<td class="my-auto ' . implode(' ', $class) . '" title="{{' . "$Entity.$name$twig" . '}}"> ';
                         //retoune que le nom du fichier
                         $tdtemp .= "{% if $Entity.$name is not empty %}<a href=\"{{asset($Entity.$name)}}\" title=\"{{TBgetFilename($Entity.$name)}}\" target=\"_blank\">{{TBgetFilename($Entity.$name)$twig|u.truncate(50, \"...\")}}</span> {% endif %}</a>";
-                        $td[] = $tdtemp . '' . "\n</td>";
+                        $td[] = $tdtemp . '' . "\n";
                         break;
                     case 'choiceenplace':
                         $twig = isset($options['twig']) ? '|' . implode('|', array_keys($options['twig'])) : '';
@@ -176,17 +176,17 @@ class CrudMakeIndexCommand extends Command
                                 {% else %}
                                     {% set numr=retour+1 %}
                                 {% endif %}
-                                <a href=\"{{path('" . $entity . "_champ',{'id':$Entity.id,'type':'" . $name . "','valeur':choice_" . $name . "|keys[numr]})}}\" style='font-size:2rem;'  title='{{ choice_" . $name . "|keys[retour]}}'> {{ choice_" . $name . "[ choice_" . $name . "|keys[retour]]$twig|raw}}</a>\n</td>";
+                                <a href=\"{{path('" . $entity . "_champ',{'id':$Entity.id,'type':'" . $name . "','valeur':choice_" . $name . "|keys[numr]})}}\" style='font-size:2rem;'  title='{{ choice_" . $name . "|keys[retour]}}'> {{ choice_" . $name . "[ choice_" . $name . "|keys[retour]]$twig|raw}}</a>\n";
                         break;
                     case 'color':
-                        $td[] = '<td class="my-auto"><div class="boxcolor" style="background-color:{{' . $Entity . '.' . $name . '}}"></div>' . "\n</td>";
+                        $td[] = '<td class="my-auto"><div class="boxcolor" style="background-color:{{' . $Entity . '.' . $name . '}}"></div>' . "\n";
                         break;
                     case 'collection':
                         //field for show
                         $return = isset($options['label']) ? $options['label'] : 'id';
                         //for separate field
                         $separation = isset($options['separation']) ? $options['separation'] : ';';
-                        $td[] = '<td class="my-auto">' . "{% for " . $name . "_item in " . $Entity . ".$name %}\n{{" . $name . "_item.$return$twig}}{{loop.last?'':'$separation'}}\n{% endfor %}" . "\n</td>";
+                        $td[] = '<td class="my-auto">' . "{% for " . $name . "_item in " . $Entity . ".$name %}\n{{" . $name . "_item.$return$twig}}{{loop.last?'':'$separation'}}\n{% endfor %}" . "\n";
                         break;
                     case 'entity':
                         //field for show
@@ -194,22 +194,22 @@ class CrudMakeIndexCommand extends Command
                         if ($docs->getType($name) == 'manytomany'  || $docs->getType($name) == 'onetomany') {
                             //for separate field
                             $separation = isset($options['separation']) ? $options['separation'] : ';';
-                            $td[] = '<td class="my-auto">' . "{% for " . $name . "_item in " . $Entity . ".$name %}\n{{" . $name . "_item.$return$twig}}{{loop.last?'':'$separation'}}\n{% endfor %}" . "\n</td>";
+                            $td[] = '<td class="my-auto">' . "{% for " . $name . "_item in " . $Entity . ".$name %}\n{{" . $name . "_item.$return$twig}}{{loop.last?'':'$separation'}}\n{% endfor %}" . "\n";
                         } else {
-                            $td[] = '<td class="my-auto">' . '{{ ' . $Entity . '.' . $name . '.' . $return . ' is defined ? ' . $Entity . '.' . $name . '.' . $return . '}}' . "\n</td>";
+                            $td[] = '<td class="my-auto">' . '{{ ' . $Entity . '.' . $name . '.' . $return . ' is defined ? ' . $Entity . '.' . $name . '.' . $return . '}}' . "\n";
                         }
                         break;
                     case 'array':
                         $return = isset($options['label']) ? array_keys($options['label'])[0] : 'id';
                         $separation = isset($options['separation']) ? $options['separation'] : ';';
-                        $td[] = '<td class="my-auto">' . "{% for " . $name . "_item in " . $Entity . ".$name %}\n{{" . $name . "_item.$return$twig}}{{loop.last?'':'$separation'}}\n{% endfor %}" . "\n</td>";
+                        $td[] = '<td class="my-auto">' . "{% for " . $name . "_item in " . $Entity . ".$name %}\n{{" . $name . "_item.$return$twig}}{{loop.last?'':'$separation'}}\n{% endfor %}" . "\n";
                         break;
                     case 'json':
                         $twig = isset($options['twig']) ? $options['twig'] : '|join(",")';
-                        $td[] = '<td class="my-auto text-center' . implode(' ', $class) . '" title="{{' . "$Entity.$name$twig" . '}}"> ' . '<i class="bi bi-zoom-in"></i>' . "\n</td>";
+                        $td[] = '<td class="my-auto text-center' . implode(' ', $class) . '" title="{{' . "$Entity.$name$twig" . '}}"> ' . '<i class="bi bi-zoom-in"></i>' . "\n";
                         break;
                     case 'slug':
-                        $td[] = '<td class="my-auto text-center clipboard' . implode(' ', $class) . '"  data-clipboard-text="{{' . "$Entity.$name$twig" . '}}" title="{{' . "$Entity.$name$twig" . '}}"> ' . '<i class="bi bi-clipboard"></i>' . "\n</td>";
+                        $td[] = '<td class="my-auto text-center clipboard' . implode(' ', $class) . '"  data-clipboard-text="{{' . "$Entity.$name$twig" . '}}" title="{{' . "$Entity.$name$twig" . '}}"> ' . '<i class="bi bi-clipboard"></i>' . "\n";
                     default:
                         if ($input->getOption('comment') != false && !in_array($name, ['updatedAt', 'createdAt', 'deletedAt'])) $output->writeln('- non géré dans makeindex:' . $select);
                         break;
@@ -238,9 +238,9 @@ class CrudMakeIndexCommand extends Command
         foreach ($timestamptable as $time) {
             if (!isset($IDOptions['tpl']['no_' . substr($time, 0, -2)])) {
                 if ($time == 'deletedAt') {
-                    $td[] .= "{%if action==\"deleted\" %}<td>{{ $Entity.$time is not empty ? $Entity.$time|date('d/m à H:i', 'Europe/Paris')}}</td>{% endif %}";
+                    $td[] .= "{%if action==\"deleted\" %}<td>{{ $Entity.$time is not empty ? $Entity.$time|date('d/m à H:i', 'Europe/Paris')}}{% endif %}";
                 } else {
-                    $td[] .= "<td>{{ $Entity.$time is not empty ? $Entity.$time|date('d/m à H:i', 'Europe/Paris'):'---'}}</td>";
+                    $td[] .= "<td>{{ $Entity.$time is not empty ? $Entity.$time|date('d/m à H:i', 'Europe/Paris'):'---'}}";
                 }
             }
         }
