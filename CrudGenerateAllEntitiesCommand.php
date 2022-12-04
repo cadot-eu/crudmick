19bed324ccd2b43cfd8e9ae02f6b3e50e1f4b983
Michael <m@cadot.eu>
2022-12-04 16:51:52 +0100

    ajout des filtres sur les images de liip

--- CrudGenerateAllEntitiesCommand.php
+++ CrudGenerateAllEntitiesCommand.php
@@ -13,58 +13,77 @@ use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use App\Service\base\ParserDocblock;
 
-#[AsCommand(
-    name: 'crud',
-    description: 'génère toutes les entités ',
-)]
+#[AsCommand(name: 'crud', description: 'génère toutes les entités ')]
 class CrudGenerateAllEntitiesCommand extends Command
 {
-    protected $entity;
+	protected string $entity;
 
-    protected function configure(): void
-    {
-        $this->addOption('comment', null, InputOption::VALUE_NONE, 'Pour afficher les commentaires');
-    }
+	protected function configure(): void
+	{
+		$this->addOption(
+			'comment',
+			null,
+			InputOption::VALUE_NONE,
+			'Pour afficher les commentaires'
+		);
+	}
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $io = new SymfonyStyle($input, $output);
-        /* -------------------------------- constant -------------------------------- */
+	protected function execute(
+		InputInterface $input,
+		OutputInterface $output
+	): int {
+		$io = new SymfonyStyle($input, $output);
+		/* -------------------------------- constant -------------------------------- */
+		$tabentities = [];
+		//secure $this->entity in minus
+		foreach (
+			array_diff(scandir('src/Entity'), [
+				'..',
+				'.',
+				'ResetPasswordRequest.php',
+				'base',
+			])
+			as $entity
+		) {
+			$tentitie = false;
+			$entity = strTolower(substr($entity, 0, -strlen('.php')));
+			$Finput = new ArrayInput([
+				'entity' => $entity,
+				'--comment' => $input->getOption('comment'),
+			]);
+			$docs = new ParserDocblock($entity);
+			$options = $docs->getOptions();
+			if ($input->getOption('comment')) {
+				$io->note('Création des fichiers de l\'entité ' . $entity . ' :');
+			}
+			if (!in_array('nocrud', $options['id'])) {
+				$init = $this->getApplication()->find('crud:init');
+				$init->run($Finput, $output);
 
-        //secure $this->entity in minus
-        foreach (array_diff(scandir('src/Entity'), array('..', '.', 'ResetPasswordRequest.php', 'base')) as $entity) {
-            $tentitie = false;
-            $entity = strTolower(substr($entity, 0, -strlen('.php')));
-            $Finput = new ArrayInput([
-                'entity' => $entity,
-                '--comment' => $input->getOption('comment'),
-            ]);
-            $docs = new ParserDocblock($entity);
-            $options = $docs->getOptions();
-            if ($input->getOption('comment')) {
-                $io->note('Création des fichiers de l\'entité ' . $entity . ' :');
-            }
-            if (!in_array('nocrud', $options['id'])) {
-                $init = $this->getApplication()->find('crud:init');
-                $init->run($Finput, $output);
+				$type = $this->getApplication()->find('crud:generate:type');
+				$type->run($Finput, $output);
+				if (!in_array('onlytype', $options['id'])) {
+					$new = $this->getApplication()->find('crud:generate:new');
+					$new->run($Finput, $output);
 
-                $type = $this->getApplication()->find('crud:generate:type');
-                $type->run($Finput, $output);
-                if (!in_array('onlytype', $options['id'])) {
-                    $new = $this->getApplication()->find('crud:generate:new');
-                    $new->run($Finput, $output);
+					$index = $this->getApplication()->find('crud:generate:index');
+					$index->run($Finput, $output);
 
-                    $index = $this->getApplication()->find('crud:generate:index');
-                    $index->run($Finput, $output);
-
-                    $controller = $this->getApplication()->find('crud:generate:controller');
-                    $controller->run($Finput, $output);
-                } else $tentitie = $entity . '(Type)';
-            } else
-                $tentitie = $entity . '(noCrud)';
-            $tabentities[] = $tentitie ?: $entity;
-        }
-        $io->success('Tous les fichiers ont été générés:' . implode(', ', $tabentities));
-        return Command::SUCCESS;
-    }
+					$controller = $this->getApplication()->find(
+						'crud:generate:controller'
+					);
+					$controller->run($Finput, $output);
+				} else {
+					$tentitie = $entity . '(Type)';
+				}
+			} else {
+				$tentitie = $entity . '(noCrud)';
+			}
+			$tabentities[] = $tentitie ?: $entity;
+		}
+		$io->success(
+			'Tous les fichiers ont été générés:' . implode(', ', $tabentities)
+		);
+		return Command::SUCCESS;
+	}
 }
--- CrudInitCommand.php
+++ CrudInitCommand.php
@@ -14,193 +14,221 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Exception;
 
-
-
-
-#[AsCommand(
-    name: 'crud:init',
-    description: 'Initialise une entity',
-)]
+#[AsCommand(name: 'crud:init', description: 'Initialise une entity')]
 class CrudInitCommand extends Command
 {
-    protected static $defaultName = 'crud:init';
-    protected function configure(): void
-    {
-        $this
-            ->addArgument('entity', InputArgument::OPTIONAL, 'nom de l\'entity')
-            ->addOption('comment', null, InputOption::VALUE_NONE, 'Pour afficher les commentaires');
-    }
+	protected static $defaultName = 'crud:init';
+	protected function configure(): void
+	{
+		$this->addArgument(
+			'entity',
+			InputArgument::OPTIONAL,
+			'nom de l\'entity'
+		)->addOption(
+			'comment',
+			null,
+			InputOption::VALUE_NONE,
+			'Pour afficher les commentaires'
+		);
+	}
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $io = new SymfonyStyle($input, $output);
-        /* --------------------------------- library -------------------------------- */
-        $helper = $this->getHelper('question');
-        /* --------------------------------- entity --------------------------------- */
-        $entity = $input->getArgument('entity');
-        if (!$entity) {
-            $question = new Question('Entrer le nom de l\'entité:');
-            $entity = $helper->ask($input, $output, $question);
-        }
-        //secure $entity in minus
-        $entity = strTolower($entity);
-        $sdir = '';
-        //création des répertoires
-        @mkdir("src/Form/$sdir/", 0777, true);
-        @mkdir("src/Controller/$sdir/", 0777, true);
-        @mkdir("templates/$sdir/$entity", 0777, true);
-        //control des paramètres et ajout si nécessaires
-        $trait = [
-            "use App\Entity\base\TimeTrait;" => "use",
-            "use Gedmo\Mapping\Annotation as Gedmo;" => "use",
-            "use Symfony\Component\Validator\Constraints as Assert;" => "use",
-            "#[ORM\HasLifecycleCallbacks()]" => "#[ORM",
-            "use TimeTrait;" => "{",
-        ];
-        $fentity = 'src/Entity/' . ucfirst($entity) . '.php';
-        foreach ($trait as $test => $comment) {
-            $Sentity = file_get_contents($fentity);
-            if (strpos($Sentity, $test) === false) {
-                $insd = strpos($Sentity, $comment);
-                $insf = strpos($Sentity, "\n", $insd);
-                file_put_contents($fentity, substr($Sentity, 0, $insf) . "\n" . $test . substr($Sentity, $insf));
-                $io->info("Paramètre `$test` ajouter dans la partie $comment ");
-            }
-        }
-        // 
-        $find = <<<'EOT'
-        public function index($search, $fields, $sort, $direction ,$categorie=null, $deleted = false, $etat = null)
-        {
-            $sort = is_null($sort) ? 'a.id' : $sort;
-            $qb = $this->createQueryBuilder('a');
-            if ($deleted) {
-                $qb->where($qb->expr()->isNotNull('a.deletedAt'));
-            } else {
-                $qb->where($qb->expr()->isNull('a.deletedAt'));
-            }
-            if ($etat != null) {
-                $qb->andwhere($qb->expr()->eq('a.etat', ':etat'))
-                    ->setParameter('etat', $etat);
-            }
-            $ORX = $qb->expr()->orx();
-            foreach ($fields as $field) {
-                $ors = [];
-                foreach (explode(' ', $search) as $s) {
-                    $s = str_replace("'", "''", $s);
-                    $ors[] = $qb->expr()->orx("a.$field LIKE '%$s%' ");
-                }
-                $ORX->add(join(' AND ', $ors));
-            }
-            $qb->andWhere($ORX);
-            if($categorie !=null)
-                $qb->andwhere($qb->expr()->isMemberOf(':categorie', 'a.categories'))->setParameter('categorie', $categorie);
+	protected function execute(
+		InputInterface $input,
+		OutputInterface $output
+	): int {
+		$io = new SymfonyStyle($input, $output);
+		/* --------------------------------- library -------------------------------- */
+		$helper = $this->getHelper('question');
+		/* --------------------------------- entity --------------------------------- */
+		$entity = $input->getArgument('entity');
+		if (!$entity) {
+			$question = new Question('Entrer le nom de l\'entité:');
+			$entity = $helper->ask($input, $output, $question);
+		}
+		//secure $entity in minus
+		$entity = strTolower($entity);
+		$sdir = '';
+		//création des répertoires
+		@mkdir("src/Form/$sdir/", 0777, true);
+		@mkdir("src/Controller/$sdir/", 0777, true);
+		@mkdir("templates/$sdir/$entity", 0777, true);
+		//control des paramètres et ajout si nécessaires
+		$trait = [
+			'use App\Entity\base\TimeTrait;' => 'use',
+			'use Gedmo\Mapping\Annotation as Gedmo;' => 'use',
+			'use Symfony\Component\Validator\Constraints as Assert;' => 'use',
+			'#[ORM\HasLifecycleCallbacks()]' => '#[ORM',
+			'use TimeTrait;' => '{',
+		];
+		$fentity = 'src/Entity/' . ucfirst($entity) . '.php';
+		foreach ($trait as $test => $comment) {
+			$Sentity = file_get_contents($fentity);
+			if (strpos($Sentity, $test) === false) {
+				$insd = strpos($Sentity, $comment);
+				$insf = strpos($Sentity, "\n", $insd);
+				file_put_contents(
+					$fentity,
+					substr($Sentity, 0, $insf) .
+						"\n" .
+						$test .
+						substr($Sentity, $insf)
+				);
+				$io->info("Paramètre `$test` ajouter dans la partie $comment ");
+			}
+		}
+		//
+		$find = <<<'EOT'
+  public function index($search, $fields, $sort, $direction ,$categorie=null, $deleted = false, $etat = null)
+  {
+      $sort = is_null($sort) ? 'a.id' : $sort;
+      $qb = $this->createQueryBuilder('a');
+      if ($deleted) {
+          $qb->where($qb->expr()->isNotNull('a.deletedAt'));
+      } else {
+          $qb->where($qb->expr()->isNull('a.deletedAt'));
+      }
+      if ($etat != null) {
+          $qb->andwhere($qb->expr()->eq('a.etat', ':etat'))
+              ->setParameter('etat', $etat);
+      }
+      $ORX = $qb->expr()->orx();
+      foreach ($fields as $field) {
+          $ors = [];
+          foreach (explode(' ', $search) as $s) {
+              $s = str_replace("'", "''", $s);
+              $ors[] = $qb->expr()->orx("a.$field LIKE '%$s%' ");
+          }
+          $ORX->add(join(' AND ', $ors));
+      }
+      $qb->andWhere($ORX);
+      if($categorie !=null)
+          $qb->andwhere($qb->expr()->isMemberOf(':categorie', 'a.categories'))->setParameter('categorie', $categorie);
 
-            return $qb->orderBy($sort, strtoupper($direction))
-                ->getQuery()
-                ->getResult();
-        }
-      EOT;
+      return $qb->orderBy($sort, strtoupper($direction))
+          ->getQuery()
+          ->getResult();
+  }
+EOT;
 
+		$repo = file_get_contents(
+			'/app/src/Repository/' . ucfirst($entity) . 'Repository.php'
+		);
+		//pon ajoute bycategorie si on a categorietrait
+		$objetEntity = 'App\Entity\\' . ucfirst($entity);
+		if (property_exists($objetEntity, 'categories')) {
+			//on ajoute le use si pas présent
+			if (strpos($repo, 'use App\Entity\Categorie;') === false) {
+				$repo = str_replace(
+					'namespace App\Repository;',
+					'namespace App\Repository;' . "\nuse App\Entity\Categorie;",
+					$repo
+				);
+			}
+		}
 
-        $repo = file_get_contents('/app/src/Repository/' . ucfirst($entity) . 'Repository.php');
-        //pon ajoute bycategorie si on a categorietrait
-        $objetEntity = 'App\Entity\\' . ucfirst($entity);
-        if (property_exists($objetEntity, 'categories')) {
-            //on ajoute le use si pas présent
-            if (strpos($repo, 'use App\Entity\Categorie;') === false) {
-                $repo = str_replace('namespace App\Repository;', 'namespace App\Repository;' . "\nuse App\Entity\Categorie;", $repo);
-            }
-        }
+		$find .= "\n" . '//fin index';
 
+		//suppression de l'ancien index
+		if (($deb = strpos($repo, 'public function index($search')) !== false) {
+			$end = strpos($repo, '//fin index', $deb);
+			file_put_contents(
+				'/app/src/Repository/' . ucfirst($entity) . 'Repository.php',
+				str_replace(
+					substr($repo, $deb, $end - $deb + strlen('//fin index')),
+					$find,
+					$repo
+				)
+			);
+		} else {
+			$end = strrpos($repo, '}');
+			$deb = $end;
+			file_put_contents(
+				'/app/src/Repository/' . ucfirst($entity) . 'Repository.php',
+				substr($repo, 0, $deb) . "\n" . $find . substr($repo, $end)
+			);
+		}
+		//file_put_contents('/app/src/Repository/' . ucfirst($entity) . 'Repository.php', substr($repo, 0, $deb) . "\n" . $find . substr($repo, $end));
 
-        $find .= "\n" . '//fin index';
+		return Command::SUCCESS;
+	}
+	public static function ArrayToKeyValue(array $array): string
+	{
+		$vars = var_export($array, true);
+		return str_replace(['¤\'', '\'¤'], '', $vars);
+	}
+	/**
+	 * It takes a filename and an array of blocks, and updates the file with the blocks
+	 *
+	 * @param string filename The name of the file to be updated.
+	 * @param array blocks an array of blocks of code
+	 */
+	public static function updateFile(string $filename, $html, $comment = false)
+	{
+		// save new file
+		$retour = file_put_contents($filename, $html);
+		if ($retour === false) {
+			throw new Exception(
+				'Erreur sur la création du fichier:' . $filename,
+				1
+			);
+		}
+		if (file_exists($filename)) {
+			if ($comment) {
+				echo 'File ' . $filename . ' généré ' . "\n";
+			}
+		}
+	}
+	/**
+	 * Method twigParser
+	 *
+	 * @param $html string twig with ¤...¤ for replacement
+	 * @param $tab array tableau des clefs à rechercher entre {{}} et à remplacer par value
+	 */
+	public static function twigParser($html, $tab): string
+	{
+		foreach ($tab as $key => $value) {
+			$html = str_replace('//¤' . $key . '¤', $value, $html); // that in first
+			$html = str_replace('¤' . $key . '¤', $value, $html);
+		}
+		return $html;
+	}
 
-        //suppression de l'ancien index
-        if (($deb = strpos($repo, 'public function index($search')) !== false) {
-            $end = strpos($repo, '//fin index', $deb);
-            file_put_contents('/app/src/Repository/' . ucfirst($entity) . 'Repository.php', str_replace(substr($repo, $deb, $end - $deb + strlen('//fin index')), $find, $repo));
-        } else {
-            $end = strrpos($repo, '}');
-            $deb = $end;
-            file_put_contents('/app/src/Repository/' . ucfirst($entity) . 'Repository.php', substr($repo, 0, $deb) . "\n" . $find . substr($repo, $end));
-        }
-        //file_put_contents('/app/src/Repository/' . ucfirst($entity) . 'Repository.php', substr($repo, 0, $deb) . "\n" . $find . substr($repo, $end));
+	/**
+	 * Given an array, find the key of an element, and another key to move before the first key.
+	 *
+	 * Return the array with the second key moved before the first key
+	 *
+	 * @param arr The array to be manipulated.
+	 * @param find The key of the element to be moved.
+	 * @param move The key of the element to be moved.
+	 *
+	 * @return The array with the element moved.
+	 */
+	public static function moveKeyBefore($arr, $find, $move)
+	{
+		if (!isset($arr[$find], $arr[$move])) {
+			return $arr;
+		}
 
+		$elem = [
+			$move => $arr[$move],
+		]; // cache the element to be moved
+		$start = array_splice($arr, 0, array_search($find, array_keys($arr)));
+		unset($start[$move]); // only important if $move is in $start
+		return $start + $elem + $arr;
+	}
 
-        return Command::SUCCESS;
-    }
-    static public function ArrayToKeyValue(array $array)
-    {
-        $vars = var_export($array, true);
-        return str_replace(['¤\'', '\'¤'], '', $vars);
-    }
-    /**
-     * It takes a filename and an array of blocks, and updates the file with the blocks
-     *
-     * @param string filename The name of the file to be updated.
-     * @param array blocks an array of blocks of code
-     */
-    static public function updateFile(string $filename, $html, $comment = false)
-    {
-        // save new file
-        $retour = file_put_contents($filename, $html);
-        if ($retour === false) {
-            throw new Exception("Erreur sur la création du fichier:" . $filename, 1);
-        }
-        if (file_exists($filename)) {
-            if ($comment) echo ('File ' . $filename . ' généré ' . "\n");
-        }
-    }
-    /**
-     * Method twigParser
-     *
-     * @param $html string twig with ¤...¤ for replacement
-     * @param $tab array tableau des clefs à rechercher entre {{}} et à remplacer par value
-     */
-    static public function twigParser($html, $tab): string
-    {
-        foreach ($tab as $key => $value) {
-            $html = str_replace('//¤' . $key . '¤', $value, $html); // that in first
-            $html = str_replace('¤' . $key . '¤', $value, $html);
-        }
-        return $html;
-    }
-
-
-    /**
-     * Given an array, find the key of an element, and another key to move before the first key.
-     *
-     * Return the array with the second key moved before the first key
-     *
-     * @param arr The array to be manipulated.
-     * @param find The key of the element to be moved.
-     * @param move The key of the element to be moved.
-     *
-     * @return The array with the element moved.
-     */
-    static public function moveKeyBefore($arr, $find, $move)
-    {
-        if (!isset($arr[$find], $arr[$move])) {
-            return $arr;
-        }
-
-        $elem = [
-            $move => $arr[$move],
-        ];  // cache the element to be moved
-        $start = array_splice($arr, 0, array_search($find, array_keys($arr)));
-        unset($start[$move]);  // only important if $move is in $start
-        return $start + $elem + $arr;
-    }
-
-
-
-    /**
-     * clean block without spaces... by trim
-     *
-     * @param  mixed $string
-     */
-    static public function clean($string): string
-    {
-        return str_replace(["\t", "\n", "\r", "\0", "\x0B", "\n", "\r", " "], '', $string);
-    }
+	/**
+	 * clean block without spaces... by trim
+	 *
+	 * @param  mixed $string
+	 */
+	public static function clean($string): string
+	{
+		return str_replace(
+			["\t", "\n", "\r", "\0", "\x0B", "\n", "\r", ' '],
+			'',
+			$string
+		);
+	}
 }
